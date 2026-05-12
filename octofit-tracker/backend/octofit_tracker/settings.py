515f4befcb082f72f60d230aba03c04b3e422409
andrei-w-accesa <andrei.wegroszta@accesa.eu>
2026-05-12 12:20:51 +0000

    Update settings and URLs to allow dynamic API root based on Codespace environment

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,13 @@ SECRET_KEY = 'django-insecure-dptnmn*ib^0%^eo00j+v@m2e0#l(m#jwaq@-@1gz0e5=ru!ixu
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+
+# Allow localhost and Codespace URLs
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
